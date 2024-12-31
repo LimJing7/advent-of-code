6f2a1f430e44e0f7b720f4c237e926c1476fa24f
Lim Jing <limjing7@gmail.com>
2025-01-01 01:10:36 +0800

    fixed day 6

--- 2024/day_06/day6.py
+++ 2024/day_06/day6.py
@@ -15,84 +15,70 @@ class Direction(IntEnum):
     DOWN = 2
     LEFT = 3
 
+def get_nxt_pos(pos, direction):
+    i, j = pos
+    direction = direction % 4
+
+    if direction == Direction.UP:
+        new_loc = (i-1, j)
+    elif direction == Direction.RIGHT:
+        new_loc = (i, j+1)
+    elif direction == Direction.DOWN:
+        new_loc = (i+1, j)
+    elif direction == Direction.LEFT:
+        new_loc = (i, j-1)
+
+    return new_loc
+
+
 input_map = {}
 seen = defaultdict(set)
-with open('test_inp.txt') as f:
+with open('input.txt') as f:
     for i, line in enumerate(f):
         for j, val in enumerate(line.strip()):
             input_map[(i,j)] = val
             if val == '^':
-                input_map[(i,j)] = 'X'
+                input_map[(i,j)] = '.'
                 start_loc = (i, j)
                 ini_dir = Direction.UP
-                seen[(i,j)].add(Direction.UP)
 
 
 print(f'{len(input_map) = }')
 sys.setrecursionlimit(len(input_map)*5)
 
-def no_block_move(inp_map, position, direction: Direction, colored: int = 1, seen=None):
-    i, j = position
-    direction = direction % 4
+def no_block_move(inp_map, position, direction: Direction, colored: int = 0, seen=None):
+    if inp_map[position] == '.':
+        inp_map[position] = 'X'
+        colored += 1
+    seen[position].add(direction % 4)
 
-    if direction == Direction.UP:
-        new_loc = (i-1, j)
-    elif direction == Direction.RIGHT:
-        new_loc = (i, j+1)
-    elif direction == Direction.DOWN:
-        new_loc = (i+1, j)
-    elif direction == Direction.LEFT:
-        new_loc = (i, j-1)
+    new_loc = get_nxt_pos(position, direction)
 
     if new_loc not in inp_map:
         return colored, False
     elif direction in seen[new_loc]:
         return colored, True
-    else:
-        seen[new_loc].add(direction)
 
     if inp_map[new_loc] == '#':
-        return no_block_move(inp_map, position, direction+1, colored, seen)
-    elif inp_map[new_loc] == '.':
-        inp_map[new_loc] = 'X'
-        return no_block_move(inp_map, new_loc, direction, colored+1, seen)
-    elif inp_map[new_loc] == 'X':
+        return no_block_move(inp_map, position, (direction+1)%4, colored, seen)
+    else:
         return no_block_move(inp_map, new_loc, direction, colored, seen)
 
-def block_move(inp_map, position, direction: Direction, colored: int = 1, seen=None):
-    i, j = position
-    direction = direction % 4
-
-    if direction == Direction.UP:
-        new_loc = (i-1, j)
-    elif direction == Direction.RIGHT:
-        new_loc = (i, j+1)
-    elif direction == Direction.DOWN:
-        new_loc = (i+1, j)
-    elif direction == Direction.LEFT:
-        new_loc = (i, j-1)
-
-    seen[new_loc].add(direction)
-
-    if new_loc not in inp_map:
-        return set()
-    elif inp_map[new_loc] == '#':
-        return block_move(inp_map, position, direction+1, colored, seen)
-    else:
-        new_map = copy.copy(inp_map)
-        new_map[new_loc] = '#'
-        new_seen = copy.copy(seen)
-        _, looped = no_block_move(new_map, position, direction+1, colored, new_seen)
-        nobs = block_move(inp_map, new_loc, direction, colored, seen)
-
-        if looped:
-            return nobs.union({new_loc})
-        else:
-            return nobs
-
-total_coloreds, looped = no_block_move(input_map, start_loc, Direction.UP, 1, seen)
+part_a_map = copy.copy(input_map)
+total_coloreds, looped = no_block_move(part_a_map, start_loc, Direction.UP, 0, seen)
 print(f'{total_coloreds = }')
 
-n_obs = block_move(input_map, start_loc, Direction.UP, 1, seen)
-print(f'{n_obs = }')
-print(f'{len(n_obs) = }')
+pos_loc = []
+for k, v in part_a_map.items():
+    if v == 'X' and k != start_loc:
+        pos_loc.append(k)
+
+n_looped = 0
+for obs in pos_loc:
+    new_map = copy.copy(input_map)
+    new_map[obs] = '#'
+    _, looped = no_block_move(new_map, start_loc, Direction.UP, 0, defaultdict(set))
+    if looped:
+        n_looped += 1
+
+print(f'{n_looped = }')
